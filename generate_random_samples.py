78ed2bf29b373008b2fabcf280badd4817c39ba1
Gang Zhang <gzhangx@hotmail.com>
2025-09-18 16:17:32 -0400

    show 100

--- generate_random_samples.py
+++ generate_random_samples.py
@@ -12,12 +12,10 @@ from common_utils import SimpleMLP
 import imageio
 
 def main():
-    from common_utils import get_device, get_model, get_norm_transform, get_raw_transform
+    from common_utils import get_device, get_model, get_raw_transform
     device = get_device()
-    raw_transform = get_raw_transform()
-    norm_transform = get_norm_transform()
-    test_raw = datasets.MNIST('.', train=False, download=True, transform=raw_transform)
-    test_norm = datasets.MNIST('.', train=False, download=True, transform=norm_transform)
+    raw_transform = get_raw_transform()    
+    test_raw = datasets.MNIST('.', train=False, download=True, transform=raw_transform)    
     model = get_model()
 
     # for each label, choose a random index with that label
@@ -25,20 +23,30 @@ def main():
     for i, (_, lbl) in enumerate(test_raw):
         labels_to_indices[lbl].append(i)
 
-    Path('outputs').mkdir(exist_ok=True)
 
-    out_dir = Path('outputs')
+    video_out_dir = Path('outputs')
+    out_dir = Path(video_out_dir / 'imgs')
+    out_dir.mkdir(exist_ok=True)
+
+    
     imgs = []
     for lbl in range(10):
         idx = random.choice(labels_to_indices[lbl])
         img_raw, _ = test_raw[idx]
-        img_norm, _ = test_norm[idx]
-        out_path = f'outputs/random_sample_{lbl}.png'
+        out_path = out_dir / f'random_sample_{idx}_{lbl}.png'
         visualize_sample(model, device, img_raw, lbl, out_path, top_k_hidden=20)
         print('Wrote', out_path)
         imgs.append(imageio.v2.imread(str(out_path)))
 
-    out_video = out_dir / 'random_samples.mp4'
+
+    for idx in range(100):        
+        img_raw, lbl = test_raw[idx]
+        out_path = out_dir / f'random_sample_{idx}_{lbl}.png'
+        visualize_sample(model, device, img_raw, lbl, out_path, top_k_hidden=20)
+        print('Wrote', out_path)
+        imgs.append(imageio.v2.imread(str(out_path)))
+
+    out_video = video_out_dir / 'random_samples.mp4'
     print('writing', out_video)
     imageio.mimwrite(str(out_video), imgs, fps=2, macro_block_size=None)
     print('Wrote', out_video)
