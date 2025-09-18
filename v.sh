4ba4ff3d6db9ac033f60157d23d2ea6445a5158c
Gang Zhang <gzhangx@hotmail.com>
2025-09-18 15:55:28 -0400

    allow index to be passed in

--- v.sh
+++ v.sh
@@ -1,3 +1,3 @@
 #/mnt/d/work/temp/aianime/.venv/bin/python train_and_visualize.py --epochs 0 --quick --load-path outputs/model.pth --input-graph 0
 
-.venv/bin/python /Users/gzhang/my/acccnaidemo/visualize_random_sample.py  --index 874
+.venv/bin/python visualize_random_sample.py --index "${1:-874}"
